661219e77ba6384cef7baee46224cdf879380d10
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-04-24 22:21:42 +0200

    example: combining, more or less done

--- projects/example-combining/src/app/store/book.effects.ts
+++ projects/example-combining/src/app/store/book.effects.ts
@@ -1,41 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, concatMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, defer } from 'rxjs';
 
 import * as BookActions from './book.actions';
 import { DataService } from '../../../../shared/data.service';
-import { ActionCreator } from '@ngrx/store';
-import { TypedAction } from '@ngrx/store/src/models';
-
 
 
 @Injectable()
 export class BookEffects {
 
-
-
   loadItems$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(BookActions.loadBooks,
              BookActions.loadAuthors,
-             BookActions.loadThumbnails
-             ),
-      concatMap(action =>
-        this.getData(action).pipe(
-          map(() => BookActions.loadBooksSuccess({ data: undefined })),
-          // catchError(error => of(BookActions.loadBooksFailure({ error }))))
-      )
+             BookActions.loadThumbnails),
+      concatMap(({ type }) => defer(() => {
+        switch (type) {
+          case BookActions.loadBooks.type: return this.service.getBooks();
+          case BookActions.loadAuthors.type: return this.service.getAuthors();
+          case BookActions.loadThumbnails.type: return this.service.getThumbnails();
+        }}).pipe(
+          map((data: any) => {
+            switch (type) {
+              case BookActions.loadBooks.type: return BookActions.loadBooksSuccess({ data });
+              case BookActions.loadAuthors.type: return BookActions.loadAuthorsSuccess({ data });
+              case BookActions.loadThumbnails.type: return BookActions.loadThumbnailsSuccess({ data });
+            }
+          }),
+          catchError(error => {
+            switch (type) {
+              case BookActions.loadBooks.type: return of(BookActions.loadBooksFailure({ error }));
+              case BookActions.loadAuthors.type: return of(BookActions.loadAuthorsFailure({ error }));
+              case BookActions.loadThumbnails.type: return of(BookActions.loadThumbnailsFailure({ error }));
+            }
+          })
+      ))
     );
   });
 
-  getData(action: TypedAction<'[Book] Load Books' | '[Book] Load Authors' | '[Book] Load Thumbnails'>) {
-    switch (action.type) {
-      case BookActions.loadBooks.type: return this.service.getBooks();
-      case BookActions.loadAuthors.type: return this.service.getAuthors();
-      case BookActions.loadThumbnails.type: return this.service.getThumbnails();
-    }
-  }
-
-  constructor(private actions$: Actions, private service: DataService) {}
+  constructor(private actions$: Actions, private service: DataService) { }
 }
